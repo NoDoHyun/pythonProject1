2853ee34f2dcd9f9383b6bc775e90da55b0ef3fe
no <uehgus@gmail.com>
2023-01-04 14:03:08 +0900

    0104

--- 0102/guilty.py
+++ 0102/guilty.py
@@ -9,14 +9,6 @@ font_path = "C:\\Windows\\Fonts\\gulim.ttc"
 font = font_manager.FontProperties(fname=font_path).get_name()
 rc('font', family=font)
 
-con = pymysql.connect(host='localhost', user='root', password='1234',
-                      db='crime', charset='utf8') # 한글처리 (charset = 'utf8')
-cur = con.cursor()
-# sql = "Select * from `crime`.`경찰청 광주광역시경찰청_자치구별 5대 범죄 현황_20211231`"#case1
-sql = "Select * from `crime`.`category`"    #case2
-cur.execute(sql)
-a= cur.fetchall()
-
 form_class = uic.loadUiType("guilist.ui")[0]
 
 #화면을 띄우는데 사용되는 Class 선언
@@ -24,11 +16,67 @@ class WindowClass(QMainWindow, form_class) :
     def __init__(self) :
         super().__init__()
         self.setupUi(self)
+        self.name=['광주북부경찰서','광주광산경찰서','광주서부경찰서','광주남부경찰서','광주동부경찰서']
+        self.name2=['경찰서','발생건수', '검거건수', '검거인원', '구속', '불구속', '기타']
         self.gwangsan.clicked.connect(self.num5)
         self.east.clicked.connect(self.num1)
         self.west.clicked.connect(self.num2)
         self.south.clicked.connect(self.num3)
         self.north.clicked.connect(self.num4)
+        self.allspace.clicked.connect(self.all)
+        self.nex.clicked.connect(self.next)
+        self.search.clicked.connect(self.fin)
+        self.lineEdit1.returnPressed.connect(self.fin2)
+        self.back1.clicked.connect(self.back)
+        self.con()
+    def con(self):
+        con = pymysql.connect(host='localhost', user='root', password='1234',
+                              db='crime', charset='utf8')  # 한글처리 (charset = 'utf8')
+        cur = con.cursor()
+        # sql = "Select * from `crime`.`경찰청 광주광역시경찰청_자치구별 5대 범죄 현황_20211231`"#case1
+        sql = "Select * from `crime`.`category`"  # case2
+        cur.execute(sql)
+        self.a = cur.fetchall()
+    def next(self):
+        self.stackedWidget.setCurrentIndex(0)
+    def fin(self):
+        self.stackedWidget.setCurrentIndex(1)
+        self.tableWidget.setRowCount(0)
+    def back(self):
+        self.stackedWidget.setCurrentIndex(3)
+    def fin2(self):
+        word=self.lineEdit1.text()
+        if word in self.name:
+            self.fill2(word)
+        else:
+            self.fill()
+
+    def fill(self):
+        count = 0
+        count3 = 1
+        lis = []
+        self.tableWidget.verticalHeader().setVisible(False)
+        self.tableWidget.setHorizontalHeaderLabels(self.name2)
+        self.tableWidget.setColumnCount(7)
+        for i in self.a:
+            lis.append(i)
+            self.tableWidget.setRowCount(count3)
+            count3+=1
+        for j in lis:
+            count2 = 0
+            for k in j:
+                self.tableWidget.setItem(count, count2, QTableWidgetItem(str(k)))
+                print(count, count2, k)
+                count2 += 1
+            count += 1
+
+    def fill2(self,word):
+        self.tableWidget.setRowCount(0)
+        for i in self.a:
+            if i[0] == word:
+                for j in range(7):
+                    self.tableWidget.setRowCount(1)
+                    self.tableWidget.setItem(0, j, QTableWidgetItem(str(i[j])))
 
     def num1(self):
         self.g(0)
@@ -43,9 +91,17 @@ class WindowClass(QMainWindow, form_class) :
     def g(self,num):
         n = num
         gra = []
-        for i in a[n]:
+        for i in self.a[n]:
             gra.append(i)
         plt.bar(['발생건수', '검거건수', '검거인원', '구속', '불구속', '기타'], [gra[1], gra[2], gra[3], gra[4], gra[5], gra[6]])
+        plt.show()
+    def all(self):
+        n=[0,1,2,3,4]
+        gra2=[]
+        for h in n:
+            i=self.a[h]
+            gra2.append(i[1])
+        plt.bar(['광주북구','광주광산구','광주서구','광주남구','광주동구'],[gra2[3],gra2[4],gra2[1],gra2[2],gra2[0]])
         plt.show()
 
 if __name__ == "__main__" :
@@ -54,6 +110,7 @@ if __name__ == "__main__" :
     myWindow = WindowClass()
     myWindow.show()
     app.exec_()
+
 # CASE 1
 # name=['광주광산경찰서','광주동부경찰서','광주서부경찰서','광주남부경찰서','광주북부경찰서']
 # b=[]
